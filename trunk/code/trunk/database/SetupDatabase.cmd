1aa0e729dd02a98be10f1849fb5b0e2992bd42b7
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-09-25 18:44:46 +0000

    # agregue los scripts para crear la base de datos

---
+++ trunk/code/trunk/database/SetupDatabase.cmd
@@ -0,0 +1,44 @@
+@ECHO OFF
+
+SETLOCAL
+%~d0
+CD "%~dp0"
+
+SET ServerAlias="%1%"
+IF "%1%"=="" SET ServerAlias=.\SQLEXPRESS
+
+ECHO.
+ECHO ==================================
+ECHO SelfManagement Database Setup 
+ECHO ==================================
+ECHO.
+
+ECHO Droping the SelfManagement database...
+@CALL SQLCMD -S %ServerAlias% -E -b -i ".\SelfManagementDB_Drop.sql"
+IF ERRORLEVEL 1 GOTO ERROR
+
+ECHO Creating the SelfManagement database...
+@CALL SQLCMD -S %ServerAlias% -E -b -i ".\SelfManagementDB_Schema.sql"
+IF ERRORLEVEL 1 GOTO ERROR
+
+ECHO.
+ECHO Setting SelfManagement database permissions...
+@CALL SQLCMD -S %ServerAlias% -E -b -i ".\SelfManagementDB_Permissions.sql"
+IF ERRORLEVEL 1 GOTO ERROR
+
+ECHO.
+ECHO ==================================
+ECHO Setup finished successfully!
+ECHO ==================================
+
+GOTO FINISH
+
+:ERROR
+
+ECHO.
+ECHO ==================================
+ECHO An error occured. 
+ECHO Please review messages above.
+ECHO ==================================
+
+:FINISH
