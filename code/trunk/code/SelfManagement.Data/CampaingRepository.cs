64d77215fb187e0563f0a936cd2a6dac43210a08
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-09-27 03:26:14 +0000

    # actualice el controller de Campañas (WIP)
    Wolof Story: https://wolof.southworksinc.com/selfmanagement/stories/04a8f700-a674-012d-d88c-12313b031b02

--- code/trunk/code/SelfManagement.Data/CampaingRepository.cs
+++ code/trunk/code/SelfManagement.Data/CampaingRepository.cs
@@ -1,6 +1,29 @@
 ﻿namespace CallCenter.SelfManagement.Data
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class CampaingRepository : ICampaingRepository
     {
+        public IList<Metric> RetrieveAvailableMetrics()
+        {
+            using (var ctx = new SelfManagementEntities())
+            {
+                var query = from metric in ctx.Metrics
+                            select metric;
+
+                return query.ToList();
+            }
+        }
+
+        public IList<Customer> SearchCustomer(string text)
+        {
+            using (var ctx = new SelfManagementEntities())
+            {
+                return ctx.Customers
+                    .FullTextSearch(text)
+                    .ToList();
+            }
+        }
     }
 }
--- code/trunk/code/SelfManagement.Data/ICampaingRepository.cs
+++ code/trunk/code/SelfManagement.Data/ICampaingRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace CallCenter.SelfManagement.Data
 {
+    using System.Collections.Generic;
+
     public interface ICampaingRepository
     {
+        IList<Metric> RetrieveAvailableMetrics();
+
+        IList<Customer> SearchCustomer(string text);
     }
 }
