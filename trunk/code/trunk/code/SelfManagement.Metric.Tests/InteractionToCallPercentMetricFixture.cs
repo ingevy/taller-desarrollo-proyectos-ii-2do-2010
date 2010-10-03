95e20f4937b1ec8cc9597ebe093b0ebb7aaf081c
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-03 23:59:15 +0000

    # Agregue 2 tests mas para la metrica I2C_PCT
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/765a2be0-ae54-012d-d908-12313b10030a

--- trunk/code/trunk/code/SelfManagement.Metric.Tests/InteractionToCallPercentMetricFixture.cs
+++ trunk/code/trunk/code/SelfManagement.Metric.Tests/InteractionToCallPercentMetricFixture.cs
@@ -28,5 +28,69 @@
             var ictMetric = new InteractionToCallPercentMetric(new DateTime());
             ictMetric.ProcessFiles(dataFiles);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowFileDateDoesNotMatchMetricDateWhenDateInDataFileDiffersFromDateInMetric()
+        {
+            var newDataFile1 = new Mock<IDataFile>();
+            newDataFile1.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.SUMMARY);
+            newDataFile1.Setup(f => f.FileDate).Returns(new DateTime(2010, 10, 2));
+
+            var dataFiles = new List<IDataFile>();
+            dataFiles.Add(newDataFile1.Object);
+
+            var ictMetric = new InteractionToCallPercentMetric(new DateTime(2010,10,3));
+            ictMetric.ProcessFiles(dataFiles);
+        }
+
+        [TestMethod]
+        public void ShouldCalculateMetricValueForTheAgentsInFile()
+        {
+            var dataLines = new List<Dictionary<string, string>>();
+
+            var dic1 = this.GenerateDictionaryForSummaryFile("34,9/8/2010,70,200,12,3,330,60,70");
+            var dic2 = this.GenerateDictionaryForSummaryFile("156,9/8/2010,50,210,8,1,320,40,70");
+            var dic3 = this.GenerateDictionaryForSummaryFile("345,9/8/2010,67,240,4,0,320,20,60");
+
+            dataLines.Add(dic1);
+            dataLines.Add(dic2);
+            dataLines.Add(dic3);
+
+            var newDataFile1 = new Mock<IDataFile>();
+            newDataFile1.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.SUMMARY);
+            newDataFile1.Setup(f => f.DataLines).Returns(dataLines);
+
+            var dataFiles = new List<IDataFile>();
+            dataFiles.Add(newDataFile1.Object);
+
+            var ictMetric = new InteractionToCallPercentMetric(new DateTime());
+            ictMetric.ProcessFiles(dataFiles);
+
+            Assert.AreEqual(3, ictMetric.CalculatedValues.Count);
+
+            var ict1 = (Convert.ToInt32(dic1["Cantidad Llamadas"]) - Convert.ToInt32(dic1["Cantidad Llamadas Transferidas"])) / Convert.ToInt32(dic1["Cantidad Llamadas"]);
+            var ict2 = (Convert.ToInt32(dic2["Cantidad Llamadas"]) - Convert.ToInt32(dic2["Cantidad Llamadas Transferidas"])) / Convert.ToInt32(dic2["Cantidad Llamadas"]);
+            var ict3 = (Convert.ToInt32(dic3["Cantidad Llamadas"]) - Convert.ToInt32(dic3["Cantidad Llamadas Transferidas"])) / Convert.ToInt32(dic3["Cantidad Llamadas"]);
+
+            Assert.AreEqual(ict1, ictMetric.CalculatedValues[Convert.ToInt32(dic1["Legajo"])]);
+            Assert.AreEqual(ict2, ictMetric.CalculatedValues[Convert.ToInt32(dic2["Legajo"])]);
+            Assert.AreEqual(ict3, ictMetric.CalculatedValues[Convert.ToInt32(dic3["Legajo"])]);
+        }
+
+        private Dictionary<string, string> GenerateDictionaryForSummaryFile(string csvData)
+        {
+            string csvColumns = "Legajo,Date,Cantidad Llamadas,Tiempo InCall (min),Tiempo en espera (min),Cantidad Llamadas Transferidas,Tiempo Loggeado (min),Tiempo Ready for Call (min),Tiempo en after call work (min)";
+            string[] columns = csvColumns.Split(',');
+            string[] values = csvData.Split(',');
+
+            var dic = new Dictionary<string, string>();
+            for (var i = 0; i < columns.Length; i++)
+            {
+                dic.Add(columns[i], values[i]);
+            }
+
+            return dic;
+        }
     }
 }
