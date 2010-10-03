321b1b525cca1df28d575e18f01d361cf8843150
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-03 20:54:24 +0000

    # Agregue el primer ejemplo de implementacion de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/765a2be0-ae54-012d-d908-12313b10030a

--- code/trunk/code/SelfManagement.Metric.Interfaces/IMetric.cs
+++ code/trunk/code/SelfManagement.Metric.Interfaces/IMetric.cs
@@ -1,12 +1,15 @@
 ﻿namespace CallCenter.SelfManagement.Metric.Interfaces
 {
     using System.Collections.Generic;
+    using System;
 
     public interface IMetric
     {
         IDictionary<int, double> CalculatedValues { get; }
 
         string ValueType { get; }
+
+        DateTime MetricDate { get; }
 
         void ProcessFiles(IList<IDataFile> dataFiles);
     }
---
+++ code/trunk/code/SelfManagement.Metric/DataFile.cs
@@ -0,0 +1,65 @@
+﻿namespace CallCenter.SelfManagement.Metric
+{
+    using CallCenter.SelfManagement.Metric.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class DataFile : IDataFile
+    {
+        private string externalSystemName;
+        private DateTime fileDate;
+        private string filePath;
+
+        public DataFile(string externalSystemName, DateTime fileDate, string filePath)
+        {
+            this.externalSystemName = externalSystemName;
+            this.fileDate = fileDate;
+            this.filePath = filePath;
+        }
+
+        public string ExternalSystemName
+        {
+            get { return this.externalSystemName; }
+        }
+
+        public System.DateTime FileDate
+        {
+            get { return this.fileDate; }
+        }
+
+        public string FilePath
+        {
+            get { return this.filePath; }
+        }
+
+        public List<string[]> DataLines
+        {
+            get
+            {
+                List<string[]> parsedData = new List<string[]>();
+
+                try
+                {
+                    using (StreamReader readFile = new StreamReader(this.FilePath))
+                    {
+                        string line;
+                        string[] row;
+
+                        while ((line = readFile.ReadLine()) != null)
+                        {
+                            row = line.Split(',');
+                            parsedData.Add(row);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new ApplicationException(e.Message);
+                }
+
+                return parsedData;
+            }
+        }
+    }
+}
