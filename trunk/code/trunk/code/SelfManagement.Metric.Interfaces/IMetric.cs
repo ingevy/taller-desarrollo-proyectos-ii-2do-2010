346ac615ac0e50eb6a7b9f2839a8a1517a04e160
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-03 18:50:22 +0000

    # Empeze con la interfaz de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/2bf58cb0-ae54-012d-d907-12313b10030a

---
+++ trunk/code/trunk/code/SelfManagement.Metric.Interfaces/IMetric.cs
@@ -0,0 +1,14 @@
+﻿namespace CallCenter.SelfManagement.Metric.Interfaces
+{
+    using System.Collections.Generic;
+    using Callcenter.SelfManagement.Metric.Interfaces;
+
+    public interface IMetric
+    {
+        void ProcessFiles(IList<IDataFile> dataFiles);
+
+        IDictionary<int, double> GetCalculatedValues();
+
+        string GetValueType();
+    }
+}
