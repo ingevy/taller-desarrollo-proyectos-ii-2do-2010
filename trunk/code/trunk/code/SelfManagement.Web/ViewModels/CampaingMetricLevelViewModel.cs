b4cdb5cea6275b13f6f277184dcefd112100773c
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-04 04:06:28 +0000

    # Avanze con la actualizacion de la pantalla de alta de campañas para soportar mulitples supervisores (WIP)
    Story_16 - Soporte para multiples Supervisores
    https://wolof.southworksinc.com/selfmanagement/stories/95e32e60-ae51-012d-d863-12313b10030a

--- trunk/code/trunk/code/SelfManagement.Web/ViewModels/CampaingMetricLevelViewModel.cs
+++ trunk/code/trunk/code/SelfManagement.Web/ViewModels/CampaingMetricLevelViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace CallCenter.SelfManagement.Web.ViewModels
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
 
     public class CampaingMetricLevelViewModel
     {
@@ -18,12 +19,28 @@
         public bool IsHighestToLowest { get; set; }
 
         [Required(ErrorMessage = "El nivel optimo de la métrica es requerido.")]
+        [CustomValidation(typeof(CampaingMetricLevelViewModel), "ValidateLevel")]
         public string OptimalLevel { get; set; }
 
         [Required(ErrorMessage = "El nivel objetivo de la métrica es requerido.")]
+        [CustomValidation(typeof(CampaingMetricLevelViewModel), "ValidateLevel")]
         public string ObjectiveLevel { get; set; }
 
         [Required(ErrorMessage = "El nivel minimo de la métrica es requerido.")]
+        [CustomValidation(typeof(CampaingMetricLevelViewModel), "ValidateLevel")]
         public string MinimumLevel { get; set; }
+
+        public static ValidationResult ValidateLevel(string level, ValidationContext validationContext)
+        {
+            var numberStyle = NumberStyles.Float | NumberStyles.Integer | NumberStyles.Number;
+            double result = 0;
+
+            if (!double.TryParse(level, numberStyle, CultureInfo.InvariantCulture, out result))
+            {
+                return new ValidationResult("Formato inválido.");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
