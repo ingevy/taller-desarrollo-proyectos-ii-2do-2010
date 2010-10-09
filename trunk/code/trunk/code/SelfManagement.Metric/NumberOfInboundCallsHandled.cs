9a88437b3700728295769e8aacd159faacf4f8d7
ingevy@gmail.com <ingevy@gmail.com@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-09 22:51:55 +0000

    # Implementacion metricas INCHAIR_OCC (13) y NCH (14)
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/2bf58cb0-ae54-012d-d907-12313b10030a

---
+++ trunk/code/trunk/code/SelfManagement.Metric/NumberOfInboundCallsHandled.cs
@@ -0,0 +1,63 @@
+﻿namespace CallCenter.SelfManagement.Metric
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using CallCenter.SelfManagement.Metric.Interfaces;
+    using System;
+    using System.Globalization;
+
+    public class NumberOfInboundCallsHandled : IMetric
+    {
+        public static double CalculateMetricValue(int cantLlamadas)
+        {
+            return (Convert.ToDouble(cantLlamadas));
+        }
+   
+        private IDictionary<int, double> calculatedValues = new Dictionary<int, double>();
+        private ExternalSystemFiles externalFileNeeded = ExternalSystemFiles.SUMMARY;
+        private DateTime metricDate;
+
+        public NumberOfInboundCallsHandled(DateTime metricDate)
+        {
+            this.metricDate = metricDate;
+        }
+
+        public IDictionary<int, double> CalculatedValues
+        {
+            get { return this.calculatedValues; }
+        }
+
+        public DateTime MetricDate
+        {
+            get { return this.metricDate; }
+        }
+
+        public void ProcessFiles(IList<IDataFile> dataFiles)
+        {
+            var metricFiles = (from f in dataFiles
+                               where f.ExternalSystemFile == this.externalFileNeeded
+                               select f).ToList<IDataFile>();
+
+            if (metricFiles.Count != 1)
+            {
+                throw new System.ArgumentException("Couldn't find necessary file to process metric"); 
+            }
+
+            if (metricFiles.First().FileDate != this.MetricDate)
+            {
+                throw new System.ArgumentException("File date does not match Metric date"); 
+            }
+
+            var dataLines = metricFiles.First().DataLines;
+
+            foreach (var line in dataLines)
+            {
+                var agentId = Convert.ToInt32(line["Legajo"]);
+                var cantLlamadas = Convert.ToInt32(line["Cantidad Llamadas"]);
+                var metricValue = NumberOfInboundCallsHandled.CalculateMetricValue(cantLlamadas);
+
+                this.calculatedValues.Add(agentId, metricValue);
+            }
+        }
+    }
+}
