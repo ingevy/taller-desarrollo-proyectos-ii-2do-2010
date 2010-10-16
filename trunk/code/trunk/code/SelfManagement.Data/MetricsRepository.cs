6f36353ec41977482d5a960ec1edbb27f3068504
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-16 19:06:16 +0000

    # Avanze con el procesamiento de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/16d53cd0-ae54-012d-d906-12313b10030a

---
+++ trunk/code/trunk/code/SelfManagement.Data/MetricsRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace CallCenter.SelfManagement.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MetricsRepository : IMetricsRepository
+    {
+
+        public ProcessedFile RetrieveProcessedFileByPath(string filePath)
+        {
+            using (var ctx = new SelfManagementEntities())
+            {
+                return ctx.ProcessedFiles
+                    .Where(pf => pf.FileSystemPath == filePath )
+                    .FirstOrDefault();
+            }
+        }
+
+        public IList<Metric> RetrieveAvailableMetrics()
+        {
+            using (var ctx = new SelfManagementEntities())
+            {
+                var query = from m in ctx.Metrics
+                            select m;
+
+                return query.ToList();
+            }
+        }
+
+        public Campaing RetrieveAgentActualCampaing(int innerUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveUserMetric(UserMetric userMetric)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveOrUpdateCampaingMetric(int campaingId, int metricId, DateTime date, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveProcessedFile(ProcessedFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
