301bf24199c811b7f8af87e0c6383ee9215fe559
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-16 23:08:47 +0000

    # Avanze con el repositorio de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/16d53cd0-ae54-012d-d906-12313b10030a

--- code/trunk/code/SelfManagement.Data/MetricsRepository.cs
+++ code/trunk/code/SelfManagement.Data/MetricsRepository.cs
@@ -31,22 +31,52 @@
 
         public Campaing RetrieveAgentActualCampaing(int innerUserId)
         {
-            throw new NotImplementedException();
+            using (var ctx = new SelfManagementEntities())
+            {
+                return ctx.Campaings
+                        .Where(c => ctx.CampaingUsers.Any(cu => (cu.CampaingId == c.Id) && (cu.InnerUserId == innerUserId) 
+                                                          && (cu.BeginDate <= DateTime.Now) && (cu.EndDate >= DateTime.Now)))
+                        .FirstOrDefault();
+            }
         }
 
-        public int SaveUserMetric(UserMetric userMetric)
+        public void CreateUserMetric(UserMetric userMetric)
         {
-            throw new NotImplementedException();
+            using (var ctx = new SelfManagementEntities())
+            {
+                ctx.UserMetrics.AddObject(userMetric);
+                ctx.SaveChanges();
+            }
         }
 
-        public int SaveOrUpdateCampaingMetric(int campaingId, int metricId, DateTime date, double value)
+        public void CreateOrUpdateCampaingMetric(int campaingId, int metricId, DateTime date, double value)
         {
-            throw new NotImplementedException();
+            using (var ctx = new SelfManagementEntities())
+            {
+                var original = ctx.CampaingMetrics
+                                  .Where(cm => (cm.CampaingId == campaingId) && (cm.MetricId == metricId) && (cm.Date == date))
+                                  .FirstOrDefault();
+
+                if (original != null)
+                {
+                    original.Value = value;
+                }
+                else
+                {
+                    ctx.CampaingMetrics.AddObject(new CampaingMetric { CampaingId = campaingId, MetricId = metricId, Date = date, Value = value });
+                }
+            }
         }
 
-        public int SaveProcessedFile(ProcessedFile file)
+        public int CreateProcessedFile(ProcessedFile file)
         {
-            throw new NotImplementedException();
+            using (var ctx = new SelfManagementEntities())
+            {
+                ctx.ProcessedFiles.AddObject(file);
+                ctx.SaveChanges();
+            }
+
+            return file.Id;
         }
     }
 }
