c31299b989bceb4bf10a2d6f1960d06a89b64bcc
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-16 20:12:20 +0000

    # Avanze con el procesamiento de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/16d53cd0-ae54-012d-d906-12313b10030a

--- trunk/code/trunk/code/SelfManagement.FilesProcessor/Program.cs
+++ trunk/code/trunk/code/SelfManagement.FilesProcessor/Program.cs
@@ -1,9 +1,13 @@
 ﻿namespace CallCenter.SelfManagement.FilesProcessor
 {
+    using CallCenter.SelfManagement.Data;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var processor = new FilesProcessor(new MetricsRepository());
+            processor.Process();
         }
     }
 }
