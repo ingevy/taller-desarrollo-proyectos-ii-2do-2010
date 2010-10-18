d1f21da12b66dcb465d6abc0c805a73c2e63f665
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-10-18 03:55:37 +0000

    # Agregue el calculo proyectado de valores de metricas
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/a234b3d0-ae57-012d-d96d-12313b10030a

---
+++ code/trunk/code/SelfManagement.Data/Helpers/MetricsCalculator.cs
@@ -0,0 +1,83 @@
+﻿namespace CallCenter.SelfManagement.Data.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MetricsCalculator
+    {
+        public double CalculateAcumulatedMetricValue(IList<UserMetric> userMetrics, DateTime date)
+        {
+            var metricValue = 0.0;
+
+            if (date <= DateTime.Now)
+            {
+                if (userMetrics.Count > 0)
+                {
+                    foreach (var um in userMetrics)
+                    {
+                        metricValue += um.Value;
+                    }
+                }
+            }
+            else
+            {
+                if (userMetrics.Count > 0)
+                {
+                    var solvr = new LeastSquareQuadraticRegression();
+                    solvr.AddPoints(Convert.ToDouble(0), Convert.ToDouble(0));
+
+                    foreach (var um in userMetrics)
+                    {
+                        metricValue += um.Value;
+                        solvr.AddPoints(Convert.ToDouble(um.Date.Day), metricValue);
+                    }
+
+                    metricValue = solvr.calculatePredictedY(Convert.ToDouble(date.Day));
+                }
+            }
+
+            return metricValue;
+        }
+
+        public double CalculateAverageMetricValue(IList<UserMetric> userMetrics, DateTime date)
+        {
+            var metricValue = 0.0;
+
+            if (date <= DateTime.Now)
+            {
+                if (userMetrics.Count > 0)
+                {
+                    foreach (var um in userMetrics)
+                    {
+                        metricValue += um.Value;
+                    }
+
+                    metricValue = metricValue / Convert.ToDouble(userMetrics.Count);
+                }
+            }
+            else
+            {
+                if (userMetrics.Count > 0)
+                {
+                    var solvr = new LeastSquareQuadraticRegression();
+                    solvr.AddPoints(Convert.ToDouble(0), Convert.ToDouble(0));
+
+                    foreach (var um in userMetrics)
+                    {
+                        metricValue += um.Value;
+                        solvr.AddPoints(Convert.ToDouble(um.Date.Day), um.Value);
+                    }
+
+                    for (var i = DateTime.Now.Day + 1; i <= date.Day; i++)
+                    {
+                        metricValue += solvr.calculatePredictedY(Convert.ToDouble(Convert.ToDouble(i)));
+                    }
+
+                    metricValue = metricValue / (Convert.ToDouble(userMetrics.Count) + Convert.ToDouble(date.Day - DateTime.Now.Day));
+                }
+            }
+
+            return metricValue;
+        }
+    }
+}
