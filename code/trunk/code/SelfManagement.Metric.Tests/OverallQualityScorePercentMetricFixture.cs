5197de76c09f4a7942bd22186dc56f3fffd92bea
ingevy@gmail.com <ingevy@gmail.com@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-11-07 23:43:16 +0000

    # Implementación metrica y Tests QA_SCORE(18)
    Story_3 - Estadisticas de Agentes
    https://wolof.southworksinc.com/selfmanagement/stories/2bf58cb0-ae54-012d-d907-12313b10030a

---
+++ code/trunk/code/SelfManagement.Metric.Tests/OverallQualityScorePercentMetricFixture.cs
@@ -0,0 +1,83 @@
+﻿namespace CallCenter.SelfManagement.Metric.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using CallCenter.SelfManagement.Metric.Interfaces;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class OverallQualityScorePercentMetricFixture
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowCouldNotFindNecessaryFileWhenSummaryFileNotInListMetricQaScore()
+        {
+            var newDataFile1 = new Mock<IDataFile>();
+            newDataFile1.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.HF);
+            var newDataFile2 = new Mock<IDataFile>();
+            newDataFile2.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.SUMMARY);
+            var newDataFile3 = new Mock<IDataFile>();
+            newDataFile3.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.STS);
+
+            var dataFiles = new List<IDataFile>();
+            dataFiles.Add(newDataFile1.Object);
+            dataFiles.Add(newDataFile2.Object);
+            dataFiles.Add(newDataFile3.Object);
+
+            var qaScoreMetric = new OverallQualityScorePercentMetric();
+            qaScoreMetric.ProcessFiles(dataFiles);
+        }
+
+        [TestMethod]
+        public void ShouldCalculateMetricValueForTheAgentsInFileMetricQaScore()
+        {
+            var dataLines = new List<Dictionary<string, string>>();
+
+            var dic1 = this.GenerateDictionaryForQAFile("34,9/8/2010,3,30,27");
+            var dic2 = this.GenerateDictionaryForQAFile("156,9/8/2010,2,20,18");
+            var dic3 = this.GenerateDictionaryForQAFile("345,9/8/2010,4,40,38");
+
+            dataLines.Add(dic1);
+            dataLines.Add(dic2);
+            dataLines.Add(dic3);
+
+            var newDataFile1 = new Mock<IDataFile>();
+            newDataFile1.Setup(f => f.ExternalSystemFile).Returns(ExternalSystemFiles.QA);
+            newDataFile1.Setup(f => f.DataLines).Returns(dataLines);
+
+            var dataFiles = new List<IDataFile>();
+            dataFiles.Add(newDataFile1.Object);
+
+            var qaScoreMetric = new OverallQualityScorePercentMetric();
+            qaScoreMetric.ProcessFiles(dataFiles);
+
+            Assert.AreEqual(3, qaScoreMetric.CalculatedValues.Count);
+
+            var qaScore1 = OverallQualityScorePercentMetric.CalculateMetricValue(Convert.ToInt32(dic1["Cantidad Puntos logrados"]), Convert.ToInt32(dic1["Cant de Puntos posibles"]));
+            var qaScore2 = OverallQualityScorePercentMetric.CalculateMetricValue(Convert.ToInt32(dic2["Cantidad Puntos logrados"]), Convert.ToInt32(dic2["Cant de Puntos posibles"]));
+            var qaScore3 = OverallQualityScorePercentMetric.CalculateMetricValue(Convert.ToInt32(dic3["Cantidad Puntos logrados"]), Convert.ToInt32(dic3["Cant de Puntos posibles"]));
+
+            Assert.AreEqual(qaScore1, qaScoreMetric.CalculatedValues[Convert.ToInt32(dic1["legajo"])]);
+            Assert.AreEqual(qaScore2, qaScoreMetric.CalculatedValues[Convert.ToInt32(dic2["legajo"])]);
+            Assert.AreEqual(qaScore3, qaScoreMetric.CalculatedValues[Convert.ToInt32(dic3["legajo"])]);
+
+            newDataFile1.VerifyAll();
+        }
+
+        private Dictionary<string, string> GenerateDictionaryForQAFile(string csvData)
+        {
+            string csvColumns = "legajo,Fecha,Cant Evaluaciones,Cant de Puntos posibles,Cantidad Puntos logrados";
+            string[] columns = csvColumns.Split(',');
+            string[] values = csvData.Split(',');
+
+            var dic = new Dictionary<string, string>();
+            for (var i = 0; i < columns.Length; i++)
+            {
+                dic.Add(columns[i], values[i]);
+            }
+
+            return dic;
+        }
+    }
+}
---
+++ code/trunk/code/SelfManagement.Metric/OverallQualityScorePercentMetric.cs
@@ -0,0 +1,56 @@
+﻿namespace CallCenter.SelfManagement.Metric
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CallCenter.SelfManagement.Metric.Interfaces;
+
+    public class OverallQualityScorePercentMetric : IMetric
+    {
+        public static double CalculateMetricValue(int cantPuntosLogrados, int cantPuntosPosibles)
+        {
+            double result = (Convert.ToDouble(cantPuntosLogrados) / Convert.ToDouble(cantPuntosPosibles));
+            return result;
+        }
+   
+        private IDictionary<int, double> calculatedValues = new Dictionary<int, double>();
+        private ExternalSystemFiles externalFileNeeded = ExternalSystemFiles.QA;
+        private DateTime metricDate;
+
+        public IDictionary<int, double> CalculatedValues
+        {
+            get { return this.calculatedValues; }
+        }
+
+        public DateTime MetricDate
+        {
+            get { return this.metricDate; }
+        }
+
+        public void ProcessFiles(IList<IDataFile> dataFiles)
+        {
+            var metricFiles = (from f in dataFiles
+                               where f.ExternalSystemFile == this.externalFileNeeded
+                               select f).ToList<IDataFile>();
+
+            if (metricFiles.Count != 1)
+            {
+                throw new System.ArgumentException("Couldn't find necessary file to process metric"); 
+            }
+
+            this.metricDate = metricFiles.First().FileDate;
+
+            var dataLines = metricFiles.First().DataLines;
+
+            foreach (var line in dataLines)
+            {
+                var agentId = Convert.ToInt32(line["legajo"]);
+                var cantPuntosPosibles = Convert.ToInt32(line["Cant de Puntos posibles"]);
+                var cantPuntosLogrados = Convert.ToInt32(line["Cantidad Puntos logrados"]);
+                var metricValue = OverallQualityScorePercentMetric.CalculateMetricValue(cantPuntosLogrados, cantPuntosPosibles);
+
+                this.calculatedValues.Add(agentId, metricValue);
+            }
+        }
+    }
+}
