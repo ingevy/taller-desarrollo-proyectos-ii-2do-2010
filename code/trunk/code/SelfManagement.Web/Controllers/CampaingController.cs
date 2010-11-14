77755c1ed8dc6fb97a7a89393afaeffe7c3225ed
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-11-14 21:00:14 +0000

    # avance con la pantalla de estadísticas globales para una campaña
    Story 5 & 6 - Pantalla de estadísticas globales de Campaña
    https://wolof.southworksinc.com/selfmanagement/stories/db6863b0-9b44-012d-f60b-12313b101d1c
    https://wolof.southworksinc.com/selfmanagement/stories/60fe8660-a0e9-012d-df1e-12313b10116c

--- code/trunk/code/SelfManagement.Web/Controllers/CampaingController.cs
+++ code/trunk/code/SelfManagement.Web/Controllers/CampaingController.cs
@@ -8,10 +8,16 @@
     using CallCenter.SelfManagement.Data;
     using CallCenter.SelfManagement.Web.Helpers;
     using CallCenter.SelfManagement.Web.ViewModels;
+    using System.Web.UI.DataVisualization.Charting;
+    using System.IO;
+    using System.Drawing;
     
     public class CampaingController : Controller
     {
         private const string CampaingMetricLevelsKey = "CampaingMetricLevels[{0}].{1}";
+
+        private readonly Color FontColor = Color.FromArgb(51, 93, 76);
+        private readonly Color BackColor = Color.FromArgb(221, 221, 238);
         private readonly ICampaingRepository campaingRepository;
         private readonly IMetricsRepository metricsRepository;
 
@@ -75,6 +81,98 @@
 
             return this.View(model);
         }
+
+
+        [Authorize(Roles = "AccountManager")]
+        public ActionResult MetricsChart(int campaingId, int metricId, string month)
+        {
+            var campaingMetric = this.campaingRepository.RetrieveCampaingMetricLevels(campaingId).FirstOrDefault(cml => cml.MetricId == metricId);
+
+            var yearMonth = month.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+            var yearNumber = int.Parse(yearMonth[0], NumberStyles.Integer, CultureInfo.InvariantCulture);
+            var monthNumber = int.Parse(yearMonth[1], NumberStyles.Integer, CultureInfo.InvariantCulture);
+
+            var beginDate = this.GetBeginDate(campaingId, yearNumber, monthNumber).Date;
+            var endDate = this.GetEndDate(campaingId, yearNumber, monthNumber).Date;
+
+            var dates = new List<DateTime>();
+
+            while (beginDate <= endDate)
+            {
+                dates.Add(beginDate.Date);
+                beginDate = beginDate.AddDays(1);
+            }
+
+            var chart = new Chart()
+            {
+                Width = 952,
+                Height = 350,
+                RenderType = RenderType.BinaryStreaming,
+                Palette = ChartColorPalette.BrightPastel,
+                BorderlineDashStyle = ChartDashStyle.Solid,
+                ToolTip = campaingMetric.Metric.MetricName,
+                BorderWidth = 2,
+                BorderlineColor = FontColor,
+                BackSecondaryColor = BackColor,
+                BackColor = BackColor,
+                ForeColor = FontColor,
+            };
+
+            chart.BorderSkin.SkinStyle = BorderSkinStyle.FrameTitle1;
+            chart.Titles.Add(new Title(campaingMetric.Metric.MetricName, Docking.Top, new Font("Trebuchet MS", 14, FontStyle.Bold), FontColor));
+            chart.Titles.Add(new Title(string.Format(CultureInfo.InvariantCulture, "Días ({0})", month), Docking.Bottom, new Font("Trebuchet MS", 12), FontColor));
+            chart.Titles.Add(new Title("Valor", Docking.Left, new Font("Trebuchet MS", 12), FontColor));
+
+            var chartArea = chart.ChartAreas.Add("Waves");
+            var legend = chart.Legends.Add("Legend");
+
+            chartArea.BackColor = BackColor;
+            legend.BackColor = BackColor;
+
+            var series1 = chart.Series.Add("Nivel Optimo");
+            var series2 = chart.Series.Add("Nivel Objetivo");
+            var series3 = chart.Series.Add("Nivel Mínimo");
+            var series4 = chart.Series.Add("Valor Métrica");
+
+            series1.ToolTip = "Nivel Optimo";
+            series1.ChartType = SeriesChartType.Line;
+            series1.BorderWidth = 3;
+            series1.ShadowOffset = 2;
+            series1.Color = Color.Green;
+
+            series2.ToolTip = "Nivel Objetivo";
+            series2.ChartType = SeriesChartType.Line;
+            series2.BorderWidth = 3;
+            series2.ShadowOffset = 2;
+            series2.Color = Color.YellowGreen;
+
+            series3.ToolTip = "Nivel Mínimo";
+            series3.ChartType = SeriesChartType.Line;
+            series3.BorderWidth = 3;
+            series3.ShadowOffset = 2;
+            series3.Color = Color.Orange;
+
+            series4.ToolTip = "Valor Métrica";
+            series4.ChartType = SeriesChartType.Line;
+            series4.BorderWidth = 3;
+            series4.ShadowOffset = 2;
+            series4.Color = Color.Blue;
+
+            foreach (var date in dates)
+            {
+                series1.Points.AddXY(date.Day, campaingMetric.OptimalLevel);
+                series2.Points.AddXY(date.Day, campaingMetric.ObjectiveLevel);
+                series3.Points.AddXY(date.Day, campaingMetric.MinimumLevel);
+                series4.Points.AddXY(date.Day, this.metricsRepository.GetCampaingMetricValue(campaingId, date, metricId));
+            }
+
+            var stream = new MemoryStream();
+            chart.SaveImage(stream, ChartImageFormat.Png);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return this.File(stream.ToArray(), "image/png");
+        }
+
 
         //
         // GET: /Campaing/Create
