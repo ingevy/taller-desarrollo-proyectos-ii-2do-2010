6eca1124e9f0d34b733df17a86e76d2e2e26076e
estelop <estelop@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-11-15 00:41:13 +0000

    # Agregue unas modificaciones al script de la BD

--- trunk/code/trunk/code/SelfManagement.Metric.Tests/DataFileFixture.cs
+++ trunk/code/trunk/code/SelfManagement.Metric.Tests/DataFileFixture.cs
@@ -5,12 +5,13 @@
     using CallCenter.SelfManagement.Metric.Interfaces;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using System.IO;
 
     [TestClass]
     public class DataFileFixture
     {
         [TestMethod]
-        public void ShouldCreateDataFileWithDateAndTypeTakenFromPath()
+        public void ShouldCreateDataFileWithDateAndTypeAndPathTakenFromPath()
         {
             var df1 = new DataFile("TTS_20101101.csv");
             var df2 = new DataFile("STS_20101101.csv");
@@ -19,6 +20,12 @@
             var df5 = new DataFile("HF System.csv");
 
             var dateExpected = new DateTime(2010, 11, 1);
+
+            Assert.AreEqual("TTS_20101101.csv", df1.FilePath);
+            Assert.AreEqual("STS_20101101.csv", df2.FilePath);
+            Assert.AreEqual("Summary_20101101.csv", df3.FilePath);
+            Assert.AreEqual("QA_20101101.csv", df4.FilePath);
+            Assert.AreEqual("HF System.csv", df5.FilePath);
 
             Assert.AreEqual(dateExpected, df1.FileDate);
             Assert.AreEqual(dateExpected, df2.FileDate);
@@ -37,7 +44,44 @@
         [ExpectedException(typeof(ArgumentException))]
         public void ShouldThrowArgumentExceptionIfFileNameNotSupported()
         {
-            var df5 = new DataFile("TTD_20101101.csv");
+            DataFile.GetDataFileType("TTZ_20101102.csv");
+        }
+
+        [TestMethod]
+        public void ShouldReturnDictionaryWithColumnValuePairsFromFile()
+        {
+            using (var sw = new StreamWriter("TTS_20100930.csv",false))
+            {
+                sw.Write("legajo,fecha Entrada,Horario Entrada,Horario Salida,fecha Salida");
+                sw.Write(Environment.NewLine);
+                sw.Write("1,14/10/2010,9:30,15:30,14/10/2010");
+                sw.Write(Environment.NewLine);
+                sw.Close();
+            }
+
+            var df1 = new DataFile("TTS_20100930.csv");
+
+            var dataLines = df1.DataLines;
+
+            foreach (var line in dataLines)
+            {
+                Assert.AreEqual("1", line["legajo"]);
+                Assert.AreEqual("14/10/2010", line["fecha Entrada"]);
+                Assert.AreEqual("9:30", line["Horario Entrada"]);
+                Assert.AreEqual("15:30", line["Horario Salida"]);
+                Assert.AreEqual("14/10/2010", line["fecha Salida"]);
+            }
+
+            File.Delete("TTS_20100930.csv");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowArgumentExceptionIfFileNotExists()
+        {
+            var df1 = new DataFile("TTS_20100929.csv");
+
+            var dataLines = df1.DataLines;
         }
     }
 }
