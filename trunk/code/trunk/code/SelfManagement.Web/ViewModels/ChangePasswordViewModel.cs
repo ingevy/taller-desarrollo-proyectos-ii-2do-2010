d76f8d001a2dcb32140d52cea6bbe5dc150c2201
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-11-21 23:45:09 +0000

    # agregué la pantalla de cambio de contraseña

--- trunk/code/trunk/code/SelfManagement.Web/ViewModels/ChangePasswordViewModel.cs
+++ trunk/code/trunk/code/SelfManagement.Web/ViewModels/ChangePasswordViewModel.cs
@@ -9,18 +9,18 @@
     {
         [Required(ErrorMessage = "La contraseña actual es requerida.")]
         [DataType(DataType.Password)]
-        [DisplayName("Contraseña actual.")]
+        [DisplayName("Contraseña actual")]
         public string OldPassword { get; set; }
 
         [Required(ErrorMessage = "La nueva contraseña es requerida.")]
         [ValidatePasswordLength]
         [DataType(DataType.Password)]
-        [DisplayName("Nueva contraseña.")]
+        [DisplayName("Nueva contraseña")]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "La confirmación de la contraseña es requerida.")]
         [DataType(DataType.Password)]
-        [DisplayName("Confirmar nueva contraseña.")]
+        [DisplayName("Confirmar nueva contraseña")]
         public string ConfirmPassword { get; set; }
     }
 }
