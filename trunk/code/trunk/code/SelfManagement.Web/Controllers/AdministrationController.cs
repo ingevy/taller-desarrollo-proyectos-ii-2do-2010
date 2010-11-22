6bcaa088a4f851b667b2cafc20016a772d8b3749
mariano.converti <mariano.converti@d552a8a4-c283-44f7-8bd1-52b8c95f3d7b>
2010-11-22 00:16:24 +0000

    # avance con el ABM de usuarios
    Story 13 - ABM de usuario
    https://wolof.southworksinc.com/selfmanagement/stories/1daa97d0-a662-012d-64b9-12313b101560

---
+++ trunk/code/trunk/code/SelfManagement.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,26 @@
+﻿namespace CallCenter.SelfManagement.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class AdministrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Users");
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        public ActionResult FileLogs()
+        {
+            return View();
+        }
+    }
+}
